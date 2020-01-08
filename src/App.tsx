90c019e955608ecbc500724291a748f1f1fe7525
Victor Ayogu <victor@seriescode.com>
2020-01-08 20:44:35 +0200

    Add function to retrieve tasks every 5 seconds

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 import MapView from "./components/MapView";
 import TaskForm from "./components/TaskForm";
+import {useGetTasks} from "./api/useGetTasks";
 
 const App: React.FC = () => {
+    const getTasks = useGetTasks();
+
+    setInterval(getTasks, 5000);
     return (
         <div className={"container"}>
             <div className="map">
