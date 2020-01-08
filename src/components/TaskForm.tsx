d0e1828f8ef71e17537afb728e871a883e3c6fb1
Victor Ayogu <victor@seriescode.com>
2020-01-08 18:32:25 +0200

    Add TaskForm styling

--- src/components/TaskForm.tsx
+++ src/components/TaskForm.tsx
@@ -4,7 +4,14 @@ import React from "react";
 const TaskForm = () => {
     return (
         <div className="task">
-            Task
+            <div className="task__header">
+                Create task
+            </div>
+
+            <input type="text" className="task__input" placeholder={"Address"}/>
+            <button className="task__button">
+                Create Task
+            </button>
         </div>)
 };
 
